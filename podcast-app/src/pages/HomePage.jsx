64343157e715f317a434b23588b99a9695272b7e
Sara Castañeda <saracastanedaecheverri@gmail.com>
2026-01-03 10:31:44 -0500

    fixing

--- podcast-app/src/pages/HomePage.jsx
+++ podcast-app/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
 import Loader from '../components/Loader'
 import {getTopPodcasts} from '../services/podcastService'
+import '../styles/HomePage.css'
 
 
 const HomePage = () => {
